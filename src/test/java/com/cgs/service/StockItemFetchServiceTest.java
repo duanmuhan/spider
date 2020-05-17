1ca79d029c5e44e4bec016b0f0a94f83045f5b8d
caoguangshu <caoguangshu@corp.netease.com>
2020-05-17 11:44:38 +0800

    提交代码

--- src/test/java/com/cgs/service/StockItemFetchServiceTest.java
+++ src/test/java/com/cgs/service/StockItemFetchServiceTest.java
@@ -1,13 +1,27 @@
 package com.cgs.service;
 
 import com.cgs.service.index.KItemFetchService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StockItemFetchServiceTest {
 
-    private KItemFetchService kItemFetchService;
+    @Autowired
+    private StockItemFetchService stockItemFetchService;
+
+    @Test
+    public void testFetchKItem(){
+        try {
+            stockItemFetchService.fetchStockList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
