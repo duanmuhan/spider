e34089ba06ffc14406b5d7ae5b6b17180cb9e39b
caoguangshu <caoguangshu@corp.netease.com>
2020-07-13 00:00:05 +0800

    提交相关代码

---
+++ src/main/java/com/cgs/dto/InstitutionalPositionDTO.java
@@ -0,0 +1,19 @@
+package com.cgs.dto;
+
+import lombok.Data;
+
+@Data
+public class InstitutionalPositionDTO {
+
+    private String rq;
+    //机构类型
+    private String jglx;
+    //持仓家数
+    private String ccjs;
+    //持仓股数(股)
+    private String ccgs;
+    //占流通股比例
+    private String zltgbl;
+    //占总股本比例
+    private String zltgbbl;
+}
---
+++ src/main/java/com/cgs/entity/InstitutionalPosition.java
@@ -0,0 +1,12 @@
+package com.cgs.entity;
+
+import lombok.Data;
+
+@Data
+public class InstitutionalPosition {
+    private String stockId;
+    private String orginationType;
+    private String numberOfHoldingPosition;
+    private String proportionOfTradableShares;
+    private String proportionInTotalEquity;
+}
