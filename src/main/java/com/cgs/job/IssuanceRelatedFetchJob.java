25455b975031d5287f5484032661194101f38b2a
caoguangshu <caoguangshu@corp.netease.com>
2020-08-30 20:41:24 +0800

    提交测试的定时job

--- src/main/java/com/cgs/job/IssuanceRelatedFetchJob.java
+++ src/main/java/com/cgs/job/IssuanceRelatedFetchJob.java
@@ -1,4 +1,19 @@
 package com.cgs.job;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@EnableScheduling
+@Slf4j
 public class IssuanceRelatedFetchJob {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void fetchTestJob(){
+        log.info("test fetchTestJob");
+    }
 }
